16c8b2c7a056e135a420f0295c07671b950f68ee
Anas Kadam <MohammedAnas.Kadam@braze.com>
2025-11-17 09:34:10 -0500

    fix kustomization

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // Change this value to change the color of the box
 // Try: "red", "blue", "green", "purple", "orange", "#FF5733", etc.
-const BoxColor = "green"
+const BoxColor = "blue"
 
 func main() {
 	http.HandleFunc("/", handleHome)
